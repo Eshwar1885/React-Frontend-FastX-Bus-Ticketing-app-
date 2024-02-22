168dd2990097de9ab64e2dc633ee851467ed82f9
eshwar <nagaeshwar1885@gmail.com>
2024-02-22 18:00:18 +0530

    buses without the type added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-// import Login from './Components/Login/Login';
+import Login from './Components/Login/Login';
 import Register from './Components/RegisterUser/Register';
 import RegisterBusOperator from './Components/RegisterBusOperator/RegisterBusOperator';
 import RegisterAdmin from './Components/RegisterAdmin/RegisterAdmin';
+import Carousel from './Components/Caraousel/Caraousel';
+import FromAndTo from './Components/FromAndTo/FromAndTo';
+import Bus from './Components/Bus/Bus';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
   return (
@@ -11,7 +18,21 @@ function App() {
             {/* <Login/> */}
             {/* <Register/> */}
             {/* <RegisterBusOperator/> */}
-            <RegisterAdmin/>
+            {/* <RegisterAdmin/> */}
+            {/* <Carousel/> */}
+            {/* <FromAndTo/> */}
+            {/* <Bus/> */}
+            <BrowserRouter>
+              <Routes>
+              <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<FromAndTo/>} />
+                {/* <Route path="/bus-list/:Origin/:Destination/:TravelDate" element={<Bus/>} /> */}
+                {/* <Route path="/buslist/:origin/:destination/:date" component={Bus} /> */}
+
+                <Route path="/bus-list" element={<Bus />} />
+              </Routes>
+            </BrowserRouter>
     </div>
   );
 }
